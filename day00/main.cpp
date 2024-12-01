b9fc7ded40a336a0105c4f0890f54294701f84a6
Alexander Samolov <alexsamolov@gmail.com>
2024-12-01 20:33:26 +0100

    adds launch configuration for vscode

--- day00/main.cpp
+++ day00/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <filesystem>
 
-int main() {
+int main(int argc, char *argv[])
+{
     std::cout << "template" << std::endl;
+    std::cout << "Current path: " << std::filesystem::current_path() << std::endl;
+    std::cout << "args:" << std::endl;
+    for (int i = 0; i < argc; i++)
+    {
+        std::cout << '\t' << argv[i] << std::endl;
+    }
     return 0;
 }
