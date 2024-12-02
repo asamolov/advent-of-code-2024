a1765e896b924446fdc4b22ab0218d277a092e7a
Alexander Samolov <alexsamolov@gmail.com>
2024-12-02 22:32:57 +0100

    day02: rewrite using vectors and nicer loop

--- day02/main.cpp
+++ day02/main.cpp
@@ -15,6 +15,37 @@ class task
 {
 private:
     std::filesystem::path file;
+    bool is_safe(const std::vector<int> &input, int exclude = -1)
+    {
+        if (input.size() < 2) {
+            return false;
+        }
+        int prev_level = -1;
+        int dir = 111;
+        for (auto it = input.cbegin(); it != input.cend(); ++it) {
+            // skip the exclude element
+            if (exclude == std::distance(input.cbegin(), it)) {
+                continue;
+            }
+            // first element
+            if (prev_level < 0) {
+                prev_level = *it;
+                continue;
+            }
+            int delta = prev_level - *it;
+            // second element
+            if (dir > 1) {
+                dir = sgn(delta); // initial direction
+            }
+            // rest
+            if (dir != sgn(delta) || std::abs(delta) < 1 || std::abs(delta) > 3)
+            {
+                return false;
+            }
+            prev_level = *it;
+        }
+        return true;
+    }
 
 public:
     task(const std::filesystem::path &input) : file(input)
@@ -30,32 +61,12 @@ public:
         while (std::getline(infile, s))
         {
             std::istringstream is(s);
-            int prev_level = 0;
-            if (!(is >> prev_level))
-            {
-                continue;
-            }
             int level = 0;
-            if (!(is >> level))
-            {
-                continue;
+            std::vector<int> levels;
+            while (is >> level) {
+                levels.push_back(level);
             }
-            int delta = prev_level - level;
-            int dir = sgn(delta);
-            bool safe = true;
-            do
-            {
-                delta = prev_level - level;
-                if (dir != sgn(delta) 
-                    || std::abs(delta) < 1 
-                    || std::abs(delta) > 3)
-                {
-                    safe = false;
-                    break;
-                }
-                prev_level = level;
-            } while (is >> level);
-            n += safe;
+            n += is_safe(levels);
         }
         return fmt::format("safe reports - {}", n);
     }
