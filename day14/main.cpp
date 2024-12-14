97fb8bfeb632aef22286dddfc4c310ecb480a2ad
Alexander Samolov <alexsamolov@gmail.com>
2024-12-14 06:29:35 +0100

    day14: part 1

--- day14/main.cpp
+++ day14/main.cpp
@@ -2,12 +2,84 @@
 #include <fstream>
 #include <filesystem>
 #include <fmt/core.h>
+#include <regex>
 #include "../utils.h"
+
+const int width = 101;
+const int height = 103;
+//const int width = 11;
+//const int height = 7;
+
+using position = std::pair<int, int>;
+using velocity = std::pair<int, int>;
+
+const std::regex r_line("p=(\\d+),(\\d+) v=(-?\\d+),(-?\\d+)");
+
+struct robot
+{
+    position pos;
+    velocity v;
+
+    robot(const std::string &s) {
+        // p=0,4 v=3,-3
+        std::smatch match;
+        std::regex_match(s, match, r_line);
+        pos = position{std::stoi(match[1]), std::stoi(match[2])};
+        v = velocity{std::stoi(match[3]), std::stoi(match[4])};
+    }
+
+    void move(int time)
+    {
+        pos.first = (time * v.first + pos.first) % width;
+        if (pos.first < 0)
+        {
+            pos.first += width;
+        }
+        pos.second = (time * v.second + pos.second) % height;
+        if (pos.second < 0)
+        {
+            pos.second += height;
+        }
+    }
+
+    int quadrant()
+    {
+        if (pos.first == width / 2 || pos.second == height / 2)
+        {
+            return 0;
+        }
+        bool left = pos.first < width / 2;
+        bool top = pos.second < height / 2;
+        if (left)
+        {
+            if (top)
+            {
+                return 1;
+            }
+            else
+            {
+                return 2;
+            }
+        }
+        else
+        {
+            if (top)
+            {
+                return 3;
+            }
+            else
+            {
+                return 4;
+            }
+        }
+    }
+};
 
 class task
 {
 private:
     std::filesystem::path file;
+
 public:
     task(const std::filesystem::path &input) : file(input)
     {
@@ -20,11 +92,17 @@ public:
         std::ifstream infile(file);
         std::string s;
         int n = 0;
+        std::vector<int> quadrants;
+        quadrants.resize(5, 0);
         while (std::getline(infile, s))
         {
-            n += s.size();
+            robot r(s);
+            r.move(100);
+            quadrants[r.quadrant()]++;
         }
-        fmt::println("n chars - {}", n);
+
+        int factor = quadrants[1] * quadrants[2] * quadrants[3] * quadrants[4];
+        fmt::println("safety factor - {}", factor);
     }
 };
 
