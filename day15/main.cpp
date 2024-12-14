30d295879941f2944dce0bbf04096eee74c310bf
Alexander Samolov <alexsamolov@gmail.com>
2024-12-15 00:08:07 +0100

    day15: template

---
+++ day15/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <fmt/core.h>
+#include "../utils.h"
+
+class task
+{
+private:
+    std::filesystem::path file;
+public:
+    task(const std::filesystem::path &input) : file(input)
+    {
+        std::cout << "Task input: " << file << std::endl;
+    }
+    void run()
+    {
+        execution_timer timer;
+        std::cout << "Starting..." << std::endl;
+        std::ifstream infile(file);
+        std::string s;
+        int n = 0;
+        while (std::getline(infile, s))
+        {
+            n += s.size();
+        }
+        fmt::println("n chars - {}", n);
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Day00" << std::endl;
+    std::cout << "Current path: " << std::filesystem::current_path() << std::endl;
+    std::cout << "args:" << std::endl;
+    for (int i = 0; i < argc; i++)
+    {
+        std::cout << '\t' << argv[i] << std::endl;
+    }
+    std::filesystem::path input;
+    if (ensure_input(argc, argv, input))
+    {
+        task task{input};
+        task.run();
+    }
+    else
+    {
+        usage(argv[0]);
+    }
+    return 0;
+}
