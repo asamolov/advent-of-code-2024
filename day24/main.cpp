948b662b64198c2fdc82ce63323521ad75f0c63a
Alexander Samolov <alexsamolov@gmail.com>
2024-12-25 00:54:51 +0100

    day24: part two. manual analysis + extra prints

--- day24/main.cpp
+++ day24/main.cpp
@@ -18,12 +18,22 @@ using gate = std::tuple<std::string, op, std::string>;
 
 const std::regex wire_exp(R"((\w\d\d): (\d))");
 const std::regex gate_exp(R"((\w+) (\w+) (\w+) -> (\w+))");
+const std::vector<std::tuple<bool, bool, bool, bool, bool>> sum_table = {
+    {false, false, false, false, false},
+    {false, true, false, true, false},
+    {false, false, true, true, false},
+    {false, true, true, false, true},
+    {true, false, false, true, false},
+    {true, true, false, false, true},
+    {true, false, true, false, true},
+    {true, true, true, true, true},
+};
 
 class task
 {
 private:
     std::filesystem::path file;
-    std::map<std::string, bool> wires;
+    std::map<std::string, bool> initial_wires;
     std::map<std::string, gate> gates;
 
 public:
@@ -31,31 +41,125 @@ public:
     {
         std::cout << "Task input: " << file << std::endl;
     }
+    void print_gates(size_t id)
+    {
+        std::string wire = fmt::format("z{:02}", id);
+        std::string exp = wire;
+        exp += " = ";
+        exp += print_gate(wire);
+        fmt::println("Gate {}", exp);
+    }
+    char print_op(op operation)
+    {
+        switch (operation)
+        {
+        case AND:
+            return '&';
+        case OR:
+            return '|';
+        case XOR:
+            return '^';
+        }
+        return 'X';
+    }
+    std::string print_gate(const std::string &wire)
+    {
+        if (!gates.contains(wire))
+        {
+            return wire;
+        }
+        const auto &[left, operation, right] = gates[wire];
+        std::string exp = "(";
+        exp += print_gate(left);
+        exp += fmt::format(" {} ", print_op(operation));
+        exp += print_gate(right);
+        exp += ")";
+        return exp;
+    }
+    bool check_gate_int(const std::map<std::string, bool> &start_wires, size_t id)
+    {
+        // zAA should be connected to xAA and yAA
+        // 1. checking that summator works
+        std::map<std::string, bool> wires = start_wires;
+        wires[fmt::format("x{:02}", id)] = false;
+        wires[fmt::format("y{:02}", id)] = false;
+        bool base_z = check_wire(wires, fmt::format("z{:02}", id));
+        for (const auto [z_1, x, y, z, z1] : sum_table)
+        {
+            if (base_z != z_1) // checking only half of the table based on previous result
+                continue;
+            std::map<std::string, bool> wires = start_wires;
+            wires[fmt::format("x{:02}", id)] = x;
+            wires[fmt::format("y{:02}", id)] = y;
+            if (z != check_wire(wires, fmt::format("z{:02}", id)) && z_1 != check_wire(wires, fmt::format("z{:02}", id + 1)))
+            {
+                print_gates(id);
+                return false;
+            }
+        }
+        return true;
+    }
+    bool check_gate(size_t id)
+    {
+        return check_gate_int(initial_wires, id);
+    }
+    bool check_gate_neg(size_t id)
+    {
+        // negating main check
+        // zAA should be connected to xAA and yAA
+        // 1. checking that summator works
+        std::map<std::string, bool> wires = initial_wires;
+        // negating all wires
+        for (auto &wire : wires)
+        {
+            wire.second = !wire.second;
+        }
 
-    bool check_wire(const std::string &wire) {
-        if (wires.contains(wire)) {
+        return check_gate_int(initial_wires, id);
+    }
+
+    bool check_wire(std::map<std::string, bool> &wires, const std::string &wire)
+    {
+        if (wires.contains(wire))
+        {
             return wires[wire];
         }
-        if (!gates.contains(wire)) {
+        if (!gates.contains(wire))
+        {
             return false;
         }
         const auto &[left, operation, right] = gates[wire];
-        auto w_left = check_wire(left);
-        auto w_right = check_wire(right);
+        auto w_left = check_wire(wires, left);
+        auto w_right = check_wire(wires, right);
         bool val;
-        switch (operation) {
-            case AND:
+        switch (operation)
+        {
+        case AND:
             val = w_left & w_right;
             break;
-            case OR:
-            val =  w_left | w_right;
+        case OR:
+            val = w_left | w_right;
             break;
-            case XOR:
+        case XOR:
             val = w_left ^ w_right;
             break;
         }
         wires[wire] = val;
         return val;
+    }
+    void validate()
+    {
+        for (size_t i = 0; i < 45; i++)
+        {
+            if (!check_gate(i))
+            {
+                fmt::println("wire z{:02} is misconnected", i);
+            }
+            else if (!check_gate_neg(i))
+            {
+                fmt::println("NEG wire z{:02} is misconnected", i);
+            }
+        }
     }
     void run()
     {
@@ -69,7 +173,7 @@ public:
             std::smatch match;
             if (std::regex_match(s, match, wire_exp))
             {
-                wires[match[1]] = match[2] != "0";
+                initial_wires[match[1]] = match[2] != "0";
             }
         }
         while (std::getline(infile, s) && !s.empty())
@@ -94,13 +198,36 @@ public:
             }
         }
         uint64_t result = 0;
+        std::map<std::string, bool> wires(initial_wires);
         for (size_t i = 0; i < 64; i++)
         {
-            result |= static_cast<uint64_t>(check_wire(fmt::format("z{:02}", i))) << i;
+            result |= static_cast<uint64_t>(check_wire(wires, fmt::format("z{:02}", i))) << i;
         }
 
         fmt::println("result {}", result);
         fmt::println("result {:#064b}", result);
+
+        for (size_t i = 0; i < 12; i++)
+        {
+            print_gates(i);
+        }
+        validate();
+        fmt::println("all zeroes wires");
+        for (size_t i = 0; i < 45; i++)
+        {
+            initial_wires[fmt::format("x{:02}", i)] = false;
+            initial_wires[fmt::format("y{:02}", i)] = false;
+        }
+
+        validate();
+        fmt::println("all ones wires");
+        for (size_t i = 0; i < 45; i++)
+        {
+            initial_wires[fmt::format("x{:02}", i)] = true;
+            initial_wires[fmt::format("y{:02}", i)] = true;
+        }
+
+        validate();
     }
 };
 
