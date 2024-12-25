64fdf66892374fdca321500cc621ae7895ae747c
Alexander Samolov <alexsamolov@gmail.com>
2024-12-25 12:08:17 +0100

    day25: final part. that was nice.

--- day25/main.cpp
+++ day25/main.cpp
@@ -2,16 +2,29 @@
 #include <fstream>
 #include <filesystem>
 #include <fmt/core.h>
+#include <fmt/ranges.h>
 #include "../utils.h"
+#include <array>
 
 class task
 {
 private:
     std::filesystem::path file;
+
 public:
     task(const std::filesystem::path &input) : file(input)
     {
         std::cout << "Task input: " << file << std::endl;
+    }
+    using pins = std::array<uint8_t, 5>;
+    bool match(const pins &lock, const pins &key)
+    {
+        for (size_t i = 0; i < lock.size(); i++)
+        {
+            if (lock[i] + key[i] > 6) // 6 due to extra pin in keys
+                return false;
+        }
+        return true;
     }
     void run()
     {
@@ -19,12 +32,39 @@ public:
         std::cout << "Starting..." << std::endl;
         std::ifstream infile(file);
         std::string s;
+        std::vector<pins> locks;
+        std::vector<pins> keys;
         int n = 0;
+        bool is_lock = false;
         while (std::getline(infile, s))
         {
-            n += s.size();
+            is_lock = s[0] == '#';
+            pins current;
+            current.fill(0);
+            while (std::getline(infile, s) && !s.empty()) // keys will have + 1 to them, it's fine.
+            {
+                for (size_t i = 0; i < current.size(); i++)
+                {
+                    current[i] += s[i] == '#';
+                }
+            }
+            if (is_lock)
+            {
+                locks.push_back(current);
+            }
+            else
+            {
+                keys.push_back(current);
+            }
         }
-        fmt::println("n chars - {}", n);
+        for (auto &lock : locks)
+        {
+            for (auto &key : keys) {
+                n += match(lock, key);
+            }
+        }
+
+        fmt::println("n pairs - {}", n);
     }
 };
 
